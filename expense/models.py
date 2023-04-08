86a0c1a121387bf8608b5039128bff3816e305bd
Thomas-Longworth <thomaslongworth555@gmail.com>
2023-04-08 21:04:49 +0000

    added create expense fucntionality

--- expense/models.py
+++ expense/models.py
@@ -6,3 +6,8 @@ class Expense(models.Model):
     name = models.CharField( max_length=50, null = False, blank = False)
 
 
+    def __str__(self):
+        return self.name
+    
+
+
--- expense/views.py
+++ expense/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Expense
 
 # Create your views here.
 
 
-
 def main(request): 
-    return render(request, 'main_page.html')
-
+    expenses = Expense.objects.all()
+    context = {
+        'Expenses': expenses
+    }
+    return render(request, 'main_page.html', context)
 
 def add(request): 
+    if request.method == "POST":
+        name = request.POST.get('expense_name')
+        Expense.objects.create(name=name)
+        return redirect('main')
     return render(request, 'add_expense.html')
 
 
