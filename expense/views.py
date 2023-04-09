7c1e0aa62b6d57dacee93059cc29af27fbea2f69
Thomas-Longworth <thomaslongworth555@gmail.com>
2023-04-09 05:43:58 +0000

    made a update expense page and added functionality to it

--- expense/views.py
+++ expense/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render, redirect
 from .models import Expense
-
+from .forms import ExpenseForm
 # Create your views here.
-
 
 def main(request): 
     expenses = Expense.objects.all()
     context = {
-        'Expenses': expenses
+        'expenses': expenses
     }
     return render(request, 'main_page.html', context)
 
 def add(request): 
     if request.method == "POST":
-        name = request.POST.get('expense_name')
-        Expense.objects.create(name=name)
-        return redirect('main')
-    return render(request, 'add_expense.html')
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+       
+      
+    form = ExpenseForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'add_expense.html', context)
 
-
+    
 def update(request): 
     return render(request, 'update_expense.html')
     
